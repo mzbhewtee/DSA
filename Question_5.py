854d73527bbc06a47ad3f2275e2b8e52152f4db2
Beauty Ikudehinbu <86835129+mzbhewtee@users.noreply.github.com>
2022-01-28 20:24:59 +0200

    Update Question_5.py

--- Question_5.py
+++ Question_5.py
@@ -27,3 +27,31 @@ def my_list(list1):
 
 list1 = [5,8,45,89,65,35,22,45]
 my_list(list1)
+
+# Question 2
+def lowercase(string):
+    print("Coverting",string,"to lowercase...\n")
+    start = timeit.default_timer()
+    print(string.lower())
+    print("Length of the initial string:", len(string))
+    end = timeit.default_timer()
+    time_elapsed = end-start
+    print("Time taken to convert to lowercase:", end - start)
+
+    # Time complexity
+    char_length = 0
+    timeusedlist = []
+    while char_length < 100:
+        if char_length == 44:
+            print(time_elapsed)
+        else:
+            time_for_list_containing_one = (char_length * time_elapsed) / 44
+        char_length += 1
+        timeusedlist.append(time_for_list_containing_one)
+
+        test_for_1million_size = (1000000 * time_elapsed) / char_length
+        estimated_time = "{:.3f}".format(test_for_1million_size)
+    print("The estimated time will be: ", estimated_time,
+          "seconds to run the algorithm\n\n")
+string = "My favouRite Song is LOSe Yourself by Eminem"
+lowercase(string)
